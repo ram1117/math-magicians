5167302c720d36c4f3ee94a0187e1b6b3cc051e4
ram kumar karuppusamy <ramkumark1@outlook.com>
2023-02-14 20:01:55 +0530

    refactor calculator component to use buttons instead of Button component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,38 +1,37 @@
-import { Button, CalcDisplay } from './Buttons';
-
 const Calculator = () => (
   <div className="calc-container">
     <>
-      <CalcDisplay value="0" />
+      <div className="calc-display">0</div>
     </>
     <>
-      <Button value="AC" />
-      <Button value="+/-" />
-      <Button value="%" />
-      <Button value="÷" className="yellow" />
+      <button className="buttons" type="button">AC</button>
+      <button className="buttons" type="button">+/-</button>
+      <button className="buttons" type="button">%</button>
+      <button className="buttons yellow" type="button">÷</button>
     </>
     <>
-      <Button value="7" />
-      <Button value="8" />
-      <Button value="9" />
-      <Button value="x" className="yellow" />
+      <button className="buttons" type="button">7</button>
+      <button className="buttons" type="button">8/-</button>
+      <button className="buttons" type="button">9</button>
+      <button className="buttons yellow" type="button">x</button>
     </>
     <>
-      <Button value="4" />
-      <Button value="5" />
-      <Button value="6" />
-      <Button value="-" className="yellow" />
+      <button className="buttons" type="button">4</button>
+      <button className="buttons" type="button">5</button>
+      <button className="buttons" type="button">6</button>
+      <button className="buttons yellow" type="button">-</button>
     </>
     <>
-      <Button value="1" />
-      <Button value="2" />
-      <Button value="3" />
-      <Button value="+" className="yellow" />
+      <button className="buttons" type="button">1</button>
+      <button className="buttons" type="button">2</button>
+      <button className="buttons" type="button">3</button>
+      <button className="buttons yellow" type="button">+</button>
     </>
     <>
-      <Button value="0" className="zero-button" />
-      <Button value="." />
-      <Button value="=" className="yellow" />
+      <button className="buttons zero-button" type="button">0</button>
+      <button className="buttons" type="button">.</button>
+      <button className="buttons yellow" type="button">=</button>
+
     </>
   </div>
 );
