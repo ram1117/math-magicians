76d25e454f954d63ad6d8571442c065cd1a72129
ram kumar karuppusamy <ramkumark1@outlook.com>
2023-02-15 13:26:55 +0530

    Add perform arithmetic operations to calculator.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,20 +1,25 @@
 import { useState } from 'react';
 import './Calculator.css';
+import calculate from '../logic/calculate';
 
 const Calculator = () => {
-  const [calcData, setCalcData] = useState(null);
+  const [calcData, setCalcData] = useState({ total: '0', next: '', operation: '' });
   const handleClick = (event) => {
-    setCalcData(event.target.textContent);
+    setCalcData(calculate(calcData, event.target.textContent));
   };
   const calcBtnText = ['AC', '+/-', '%', '÷', '7', '8', '9',
     'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
   return (
-    <div className="calc-container" role="button" tabIndex={0} onClick={handleClick} onKeyDown={() => { }}>
+    <div className="calc-container">
       <>
-        <div className="calc-display">{calcData}</div>
+        <div className="calc-display">
+          {calcData.total}
+          {calcData.operation}
+          {calcData.next}
+        </div>
       </>
       {
-        calcBtnText.map((item) => <button className="buttons" key={item} type="button">{item}</button>)
+        calcBtnText.map((item) => <button className="buttons" onClick={(event) => { handleClick(event); }} key={item} type="button">{item}</button>)
       }
     </div>
   );
