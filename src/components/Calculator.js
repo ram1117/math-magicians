4bb8e34325c30819674b97251636ecf303b1a9bf
ram kumar karuppusamy <ramkumark1@outlook.com>
2023-02-15 13:13:29 +0530

    add calculate and operate logic, add big-js to project dependency

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,16 +1,17 @@
+import { useState } from 'react';
 import './Calculator.css';
 
 const Calculator = () => {
+  const [calcData, setCalcData] = useState(null);
   const handleClick = (event) => {
-    console.log(event.target.textContent);
+    setCalcData(event.target.textContent);
   };
-
   const calcBtnText = ['AC', '+/-', '%', '÷', '7', '8', '9',
     'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
   return (
     <div className="calc-container" role="button" tabIndex={0} onClick={handleClick} onKeyDown={() => { }}>
       <>
-        <div className="calc-display">0</div>
+        <div className="calc-display">{calcData}</div>
       </>
       {
         calcBtnText.map((item) => <button className="buttons" key={item} type="button">{item}</button>)
